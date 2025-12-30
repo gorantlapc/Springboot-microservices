87ea24fd9d6924d0cd4f25b5e4b5969e7b860cb0
Punnaiah Chowdary Gorantla <pcatgothenburg@gmail.com>
2025-12-30 21:33:24 +0100

    Removed service-registry(eureka-server) service and updated files accordingly

--- backend/order-service/src/main/java/com/gorantla/orderservice/OrderServiceApplication.java
+++ backend/order-service/src/main/java/com/gorantla/orderservice/OrderServiceApplication.java
@@ -2,7 +2,6 @@ package com.gorantla.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -12,11 +11,9 @@ import org.springframework.web.client.RestTemplate;
 public class OrderServiceApplication {
 
 	@Bean
-	@LoadBalanced  // Important if calling services registered in Eureka
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
-
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrderServiceApplication.class, args);
