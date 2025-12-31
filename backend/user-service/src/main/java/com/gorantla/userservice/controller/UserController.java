440b18df88cd7dd44456b4e51fbf823d6e338831
Punnaiah Chowdary Gorantla <pcatgothenburg@gmail.com>
2025-12-31 14:00:15 +0100

    added log message in user service

--- backend/user-service/src/main/java/com/gorantla/userservice/controller/UserController.java
+++ backend/user-service/src/main/java/com/gorantla/userservice/controller/UserController.java
@@ -1,15 +1,21 @@
 package com.gorantla.userservice.controller;
 
 import com.gorantla.userservice.dto.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
 
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
+
     @GetMapping("/greet/{message}")
-    public String greet(@PathVariable String message) {
-        return "Hi " + message;
+    public ResponseEntity<String> greet(@PathVariable String message) {
+        log.debug("Greet endpoint called with message: {}", message);
+        return ResponseEntity.ok("Hi " + message);
     }
 
     @PostMapping("/register")
