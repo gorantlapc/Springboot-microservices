4f9fd02103723a1db1f597c15d3be13301b8c0d6
Punnaiah Chowdary Gorantla <pcatgothenburg@gmail.com>
2026-01-08 22:00:33 +0100

    Removed the circuit breaker logic. will add that logic at Dapr config

--- backend/order-service/src/main/java/com/gorantla/orderservice/service/OrderService.java
+++ backend/order-service/src/main/java/com/gorantla/orderservice/service/OrderService.java
@@ -4,15 +4,9 @@ import com.gorantla.orderservice.data.Message;
 import com.gorantla.orderservice.data.Order;
 import com.gorantla.orderservice.data.OrderStatus;
 import com.gorantla.orderservice.data.PaymentResponse;
-import com.gorantla.orderservice.exception.ServiceUnavailableException;
-import io.github.resilience4j.circuitbreaker.CallNotPermittedException;
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.HttpServerErrorException;
-import org.springframework.web.client.ResourceAccessException;
-import org.springframework.web.client.RestTemplate;
 
 @Service
 public class OrderService {
@@ -23,16 +17,14 @@ public class OrderService {
 
     private final InventoryClient inventoryClient;
 
-    public OrderService(RestTemplate restTemplate, PaymentClient paymentClient, InventoryClient inventoryClient) {
+    public OrderService(PaymentClient paymentClient, InventoryClient inventoryClient) {
         this.paymentClient = paymentClient;
         this.inventoryClient = inventoryClient;
     }
 
-    @CircuitBreaker(name = "myCircuitBreaker", fallbackMethod = "fallbackMethod")
     public Message processOrder(Order order) {
 
         log.debug("Processing order: {}", order);
-
         // Call the inventory service to check and reserve stock
         Boolean isInStock = inventoryClient.isInStock(order.productCode()).block();
         if (Boolean.FALSE.equals(isInStock)) {
@@ -49,11 +41,4 @@ public class OrderService {
         return topicMessage;
     }
 
-    public String fallbackMethod(Throwable throwable) {
-        if (throwable instanceof ResourceAccessException || throwable instanceof CallNotPermittedException
-                || throwable instanceof HttpServerErrorException) {
-            throw new ServiceUnavailableException("Payment service is currently unavailable.", throwable);
-        }
-        throw new ServiceUnavailableException("Payment service is currently unavailable.", throwable);
-    }
 }
